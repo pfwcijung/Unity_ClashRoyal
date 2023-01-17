86acd74bc87b8edd36fb952e3f2d2ebf7f4e8390
pfwcijung <39897425+pfwcijung@users.noreply.github.com>
2023-01-17 21:51:48 +0900

    01/17

--- Assets/1.Scripts/CardController.cs
+++ Assets/1.Scripts/CardController.cs
@@ -7,22 +7,48 @@ public class CardController : MonoBehaviour
     [SerializeField] private CardUI prefab;
     [SerializeField] private Transform tempParent;
     [SerializeField] public Transform cardTempParent;
-    //°í¹Î
-    public GameObject[] cards = new GameObject[4];
+    public List<CardUI> cards = new List<CardUI>(); 
 
     float delayTime = 0;
+    public int cardIdx = 0;
+    private int[] ableCard = new int[4];
 
+    void Start()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            CardUI card = Instantiate(prefab, tempParent);
+            card.SetParent(cardTempParent);
+            card.SetIndex(cardIdx);
+            cards.Add(card);
+            ableCard[cardIdx] = 1;
+            cardIdx++;
+            delayTime = 0;
+        }     
+    }
     void Update()
     {
-        if (cards.Length > 4)
-            return;
-
-        delayTime += Time.deltaTime;
-
-        if (delayTime >= 1f)
+        for(int i = 0; i < 4; i++)
         {
-            Instantiate(prefab, tempParent).SetParent(cardTempParent);
-            delayTime = 0;
+            if (ableCard[i] == 0)
+            {
+                delayTime += Time.deltaTime;
+
+                if (delayTime >= 1f)
+                {
+                    CardUI card = Instantiate(prefab, tempParent);
+                    card.SetParent(cardTempParent);
+                    cards.Add(card);
+                    delayTime = 0;
+                    ableCard[i] = 1;
+                }
+            }
         }
+    }
+
+    public void DestroyCard(int index)
+    {
+        cards.RemoveAt(index);
+        ableCard[index] = 0;
     }
 }
--- Assets/1.Scripts/CardUI.cs
+++ Assets/1.Scripts/CardUI.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CardUI : MonoBehaviour
 {
     [SerializeField] private Transform prefab;
     [SerializeField] private Transform tempParent;
+    [SerializeField] TMP_Text costTxt;
 
+    public int CardIndex = 0;
+    public int cost = 0;
     void Start()
     {
+        cost = Random.Range(1, 9);
+    }
 
+    void Update()
+    {
+        costTxt.text = string.Format($"{cost}");
     }
 
     void SpawnMonster()
@@ -22,8 +32,17 @@ public class CardUI : MonoBehaviour
         tempParent = trans;
     }
 
+    public void SetIndex(int index)
+    {
+        CardIndex = index;
+    }
+
     public void OnClick()
     {
         SpawnMonster();
+
+        ControllerManager.Instance.cardCont.DestroyCard(CardIndex);
+
+        Destroy(gameObject);
     }
 }
